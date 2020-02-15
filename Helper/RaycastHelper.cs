a739f6134a86b3121c2a1d841e9a7a00999c75d5
X-Doctor-X <57560155+X-Doctor-X@users.noreply.github.com>
2020-02-15 09:53:38 -0300

    Add files via upload

---
+++ Helper/RaycastHelper.cs
@@ -0,0 +1,38 @@
+﻿using SDG.Framework.Landscapes;
+using SDG.Unturned;
+using System.Linq;
+using UnityEngine;
+
+namespace XPlugins.RealisticVehiclesLock
+{
+    public class RaycastHelper
+    {
+        public static RaycastHit[] RaycastAll(Ray ray, float maxDistance, int layerMask, QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
+        {
+            if ((layerMask & RayMasks.GROUND) == RayMasks.GROUND)
+            {
+                LandscapeHoleUtility.raycastIgnoreLandscapeIfNecessary(ray, maxDistance, ref layerMask);
+            }
+            return Physics.RaycastAll(ray, maxDistance, layerMask, queryTriggerInteraction);
+        }
+
+        public static InteractableVehicle GetVehicleFromHits(Player caller, RaycastHit[] hits)
+        {
+            InteractableVehicle vehicle = null;
+            int hitsCount = hits.Count();
+            if (hitsCount > 0)
+            {
+                for (int i = 0; i < hitsCount; i++)
+                {
+                    InteractableVehicle vv = hits[i].transform.GetComponentInParent<InteractableVehicle>();
+                    if (vv != caller)
+                    {
+                        vehicle = vv;
+                        break;
+                    }
+                }
+            }
+            return vehicle;
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Rocket.Core.Plugins;
+using SDG.Unturned;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
+using Rocket.Unturned.Player;
+using Steamworks;
+using System.Linq;
+using Rocket.Unturned.Events;
+using Rocket.API;
+
+namespace XPlugins.RealisticVehiclesLock
+{
+    public class Main : RocketPlugin
+    {
+        public static Main Instance;
+        protected override void Load()
+        {
+            Instance = this;
+            Logger.Log("###########################################", ConsoleColor.Green);
+            Logger.Log("#[RealisticVehiclesLock] Loaded correctly!#", ConsoleColor.Green);
+            Logger.Log("###########################################", ConsoleColor.Green);
+
+            VehicleManager.onEnterVehicleRequested += OnEnterVehicle;
+            VehicleManager.onExitVehicleRequested += OnLeaveVehicle;
+            UnturnedPlayerEvents.OnPlayerUpdateGesture += OnGesture;
+        }
+
+        private void OnLeaveVehicle(Player player, InteractableVehicle vehicle, ref bool cancel, ref Vector3 pendingLocation, ref float pendingYaw)
+        {
+            if (vehicle.isLocked == false)
+            {
+                return;
+            }
+            else
+            {
+                cancel = false;
+                ChatManager.say("[RealisticVehiclesLock] This vehicle is locked, you need to unlock it to leave this vehicle!", Color.red, false);
+            }
+        }
+
+        private void OnEnterVehicle(Player player, InteractableVehicle vehicle, ref bool cancel)
+        {
+            if (vehicle.isLocked == false)
+            {
+                return;
+            }
+            else if(vehicle.isLocked == true)
+            {
+                cancel = false;
+                ChatManager.say("[RealisticVehiclesLock] This vehicle is locked, you need to unlock it to get in this vehicle!", Color.red, false);
+            }
+        }
+
+        private void OnGesture(UnturnedPlayer player, UnturnedPlayerEvents.PlayerGesture gesture)
+        {
+            if (gesture == UnturnedPlayerEvents.PlayerGesture.PunchLeft | gesture == UnturnedPlayerEvents.PlayerGesture.PunchRight)
+            {
+                Player user = player.Player;
+
+                InteractableVehicle vehicle1 = RaycastHelper.GetVehicleFromHits(user, RaycastHelper.RaycastAll(new Ray(user.look.aim.position, user.look.aim.forward), 4f, RayMasks.VEHICLE));
+
+                if (vehicle1 != null)
+                {
+                    if ((vehicle1.lockedOwner == player.CSteamID || vehicle1.lockedGroup == player.SteamGroupID) && vehicle1.isLocked == true)
+                    {
+                        VehicleManager.instance.channel.send("tellVehicleLock", ESteamCall.ALL, ESteamPacket.UPDATE_RELIABLE_BUFFER, vehicle1.instanceID, player.CSteamID, player.SteamGroupID, false);
+                    }
+                    else if ((vehicle1.lockedOwner == player.CSteamID || vehicle1.lockedGroup == player.SteamGroupID) && vehicle1.isLocked == false)
+                    {
+                        VehicleManager.instance.channel.send("tellVehicleLock", ESteamCall.ALL, ESteamPacket.UPDATE_RELIABLE_BUFFER, vehicle1.instanceID, player.CSteamID, player.SteamGroupID, true);
+                    }
+                }
+            }
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("#############################################", ConsoleColor.Green);
+            Logger.Log("#[RealisticVehiclesLock] Unloaded correctly!#", ConsoleColor.Green);
+            Logger.Log("#############################################", ConsoleColor.Green);
+        }
+    }
+}
